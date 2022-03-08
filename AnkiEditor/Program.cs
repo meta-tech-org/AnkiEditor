9e9f15a4c238da51f7c088d32ae0b26fb64d2a6e
Julius Jacobsohn <julius.jacobsohn@udo.edu>
2022-03-08 14:31:21 +0100

    Add code that adds empty decks

--- AnkiEditor/Program.cs
+++ AnkiEditor/Program.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace AnkiEditor
 {
@@ -13,13 +14,28 @@ namespace AnkiEditor
 
             // Import
             Deck root = JsonConvert.DeserializeObject<Deck>(File.ReadAllText(deckPath));
+            foreach(var chapter in root.children)
+            {
+                foreach(var letter in chapter.children)
+                {
+                    if(letter.children.Count > 0)
+                    {
+                        continue;
+                    }
+                    letter.children.Add(CreateEmptyDeck("01 Kana", "268c3ebf-2d30-11ec-b5f4-0c7a15ee466f"));
+                    letter.children.Add(CreateEmptyDeck("02 Vokabeln", "268c3ebf-2d30-11ec-b5f4-0c7a15ee466f"));
+                    letter.children.Add(CreateEmptyDeck("03 Grammatik", "268c3ebf-2d30-11ec-b5f4-0c7a15ee466f"));
+                    letter.children.Add(CreateEmptyDeck("04 Sätze und Phrasen", "268c3ebf-2d30-11ec-b5f4-0c7a15ee466f"));
+                    letter.children.Add(CreateEmptyDeck("05 Trivia", "268c3ebf-2d30-11ec-b5f4-0c7a15ee466f"));
+                }
+            }
 
             // Export
             string[] lines = JsonConvert.SerializeObject(root, new JsonSerializerSettings
             {
-                Formatting = Formatting.Indented,
-                NullValueHandling = NullValueHandling.Ignore,
-                
+                Formatting = Formatting.Indented, //Try to match CrowdAnki export
+                NullValueHandling = NullValueHandling.Ignore, //Try to match CrowdAnki export
+
             }).Split("\r\n");
             for (int i = 0; i < lines.Length; i++)
             {
@@ -41,5 +57,28 @@ namespace AnkiEditor
             string resultString = string.Join("\r\n", lines);
             File.WriteAllText(deckPath, resultString);
         }
+
+        static Deck CreateEmptyDeck(string name, string configId)
+        {
+            return new Deck
+            {
+                children = new System.Collections.Generic.List<Deck>(),
+                crowdanki_uuid = Guid.NewGuid().ToString().ToLower(),
+                deck_configurations = null,
+                deck_config_uuid = configId,
+                desc = "",
+                dyn = 0,
+                extendNew = 0,
+                extendRev = 0,
+                media_files = new System.Collections.Generic.List<string>(),
+                mid = null,
+                name = name,
+                notes = new System.Collections.Generic.List<Note>(),
+                note_models = null,
+                __type__ = "Deck"
+            };
+        }
     }
+
+
 }
