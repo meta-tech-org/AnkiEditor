910997a49f8c5ef68c79373a12fd800ffd58390b
Julius Jacobsohn <julius.jacobsohn@udo.edu>
2022-03-08 13:59:22 +0100

    Fix space bug

--- AnkiEditor/Program.cs
+++ AnkiEditor/Program.cs
@@ -15,6 +15,24 @@ namespace AnkiEditor
             Deck root = JsonConvert.DeserializeObject<Deck>(File.ReadAllText(deckPath));
 
             // Export
+            string[] lines = JsonConvert.SerializeObject(root, Formatting.Indented).Split("\r\n");
+            for(int i = 0; i < lines.Length; i++)
+            {
+                var lineSpaces = 0;
+                var lineParts = lines[i].Split(" ");
+                foreach(var linePart in lineParts)
+                {
+                    if (linePart == "")
+                    {
+                        lineSpaces++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                lines[i] = lines[i].PadLeft(lines[i].Length + lineSpaces, ' ');
+            }
             File.WriteAllText(deckPath, JsonConvert.SerializeObject(root, Formatting.Indented).Replace("  ","    "));
         }
     }
