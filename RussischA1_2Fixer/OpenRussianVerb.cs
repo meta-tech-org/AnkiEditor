c174e2fca7dbb9ad3838f58b7e5f0187019765ac
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2022-07-03 14:38:17 +0200

    Add naive model for russian verbs

---
+++ RussischA1_2Fixer/OpenRussianVerb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussischA1_2Fixer
+{
+    public class OpenRussianVerb
+    {
+        public int id { get; set; }
+        public string accented { get; set; }
+        public string bare => accented.Replace("'", "");
+        //public string aspect { get; set; }
+        //public string partner { get; set; }
+        //public string imperative_sg { get; set; }
+        //public string imperative_pl { get; set; }
+        //public string past_m { get; set; }
+        //public string past_f { get; set; }
+        //public string past_n { get; set; }
+        //public string past_pl { get; set; }
+        public string sg1 { get; set; }
+        public string sg2 { get; set; }
+        public string sg3 { get; set; }
+        public string pl1 { get; set; }
+        public string pl2 { get; set; }
+        public string pl3 { get; set; }
+    }
+}
