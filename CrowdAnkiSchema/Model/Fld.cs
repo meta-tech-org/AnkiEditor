5d239a6c064834587b6c3b34909165d771b3496a
Julius Jacobsohn <julius.jacobsohn@hotmail.com>
2023-03-10 12:50:57 +0100

    Add new anki fields

--- CrowdAnkiSchema/Model/Fld.cs
+++ CrowdAnkiSchema/Model/Fld.cs
@@ -8,10 +8,12 @@ namespace CrowdAnkiSchema.Model
 {
     public class Fld
     {
+        public bool? collapsed { get; set; }
         public string description { get; set; }
         public string font { get; set; }
         public string name { get; set; }
         public int ord { get; set; }
+        public bool? plainText { get; set; }
         public bool rtl { get; set; }
         public int size { get; set; }
         public bool sticky { get; set; }
